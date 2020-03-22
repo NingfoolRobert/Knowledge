a871a68cfcbdef012159bc4d91c5871fd53d5bc0
NingfoolRobert <371494724@qq.com>
2020-03-22 11:30:55 +0800

    feat get TimerCount

---
+++ Test/Timer/Test.cpp
@@ -0,0 +1,19 @@
+
+#include <string> 
+#include <stdlib.h> 
+#include <stdio.h> 
+
+
+#define rdtscll(val)  __asm__ __volatile__("rdtsc" : "=A" (val)) 
+
+
+
+int main()
+{
+	long val = 0;
+	rdtscll(val);
+
+	printf("%ld\n", val);
+
+	return 0;
+}
