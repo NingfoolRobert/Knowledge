26826714318e0bed3916173521b7ea01ea510528
NingfoolRobert <371494724@qq.com>
2020-07-05 20:32:09 +0800

    delete file

--- Base/GlobalVar.cpp
+++
@@ -1,8 +0,0 @@
-
-#include "GlobalVar.h"
-
-char m_szServiceName[BUFLEN_SERVICE_NAME];
-
-char m_szConfigureFileName[MAX_PATH];
-
-char m_szServiceLogFileName[MAX_PATH];
--- Base/TestMain.cpp
+++
@@ -1,9 +0,0 @@
-#include "ServiceTpl.h"
-#include "TestService.h"
-
-int main(int argc, char** argv)
-{
-	CServiceTpl<CTestService> service;
-	service.Execute("Test");
-	return 0;	
-}
