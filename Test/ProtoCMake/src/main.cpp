a7cc0ac09e80feead41bf7bad5ab1f0610203415
NingfoolRobert <371494724@qq.com>
2020-08-23 23:32:08 +0800

    feat: proto src file generate the file of .h and .cc by the cmake

---
+++ Test/ProtoCMake/src/main.cpp
@@ -0,0 +1,22 @@
+
+#include "person.pb.h"
+
+#include <iostream> 
+#include <stdlib.h> 
+#include <stdio.h> 
+
+
+
+int main()
+{
+	using namespace PB;
+	 
+	person stInfo; 
+	stInfo.set_uid(10);
+	stInfo.set_uname("Tom");
+
+
+
+	
+	return 0;
+}
