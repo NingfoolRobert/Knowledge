3ec6b944ebee9b18681afc78c002614a73bf1286
NingfoolRobert <371494724@qq.com>
2020-10-25 17:03:11 +0800

    feat(reactor): base event loop

---
+++ reactor/EventLoop.h
@@ -0,0 +1,31 @@
+/** 
+ * Copyright (C) 2020 xxx.
+ * 
+ * Unpublished copyright. All rights reserved. This material contains
+ * proprietary information that should be used or copied only within
+ * xxx, except with written permission of xxx.
+ * 
+ * @file:xxxEventLoop.h
+ * @brief:xxxEventLoop 
+ * @author:xxxEventLoopnbf,nbf0537@163.com
+ * @version:com 1.0
+ * @date:com2020-10-25
+ */
+
+#pragma once 
+
+#include <stdio.h> 
+#include <stdlib.h> 
+
+
+
+class EventLoop 
+{
+public:
+	EventLoop();
+	virtual ~EventLoop();
+public:
+	
+
+
+};
