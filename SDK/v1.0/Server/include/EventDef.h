bef7198230d77910e5e83d43936bcbaa406f9da2
ningbeifei <Beifei.Ning@cicc.com.cn>
2020-12-23 17:39:09 +0800

    feat(Event): unsigned long

--- SDK/v1.0/Server/include/EventDef.h
+++ SDK/v1.0/Server/include/EventDef.h
@@ -5,11 +5,11 @@
  * proprietary information that should be used or copied only within
  * xxx, except with written permission of xxx.
  * 
- * @file:xxxEventDef.h
- * @brief:xxxEventDef 
- * @author:xxxEventDefnbf,nbf0537@163.com
- * @version:com 1.0
- * @date:com2020-04-21
+ * @file:EventDef.h
+ * @brief:EventDef 
+ * @author:nbf,nbf0537@163.com
+ * @version:1.0
+ * @date:2020-04-21
  */
 
 
@@ -18,7 +18,8 @@
 
 typedef struct tagEventHeader 
 {
-	unsigned int dwType;
-	unsigned int dwLength;
-	unsigned int dwParam; 
+	unsigned int	dwType;
+	unsigned int	dwLength;
+	unsigned int	dwParam; 
+	unsigned long	ulParam;
 }EVENTHEADER, *PEVENTHEADER;
